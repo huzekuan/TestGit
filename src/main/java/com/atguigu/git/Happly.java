e2e4f3b544e15b2609a40b66bac1869ccc1d700a
tom_glb <goobMorning_glb@atguigu.com>
2019-05-08 08:37:27 +0800

    commit test host_fix

--- src/main/java/com/atguigu/git/Happly.java
+++ src/main/java/com/atguigu/git/Happly.java
@@ -1,5 +1,9 @@
 package com.atguigu.git;
 
 public class Happly {
-
+   public static void main(String[] args){
+	   System.out.println("left...");
+	   System.out.println("hot fix...");
+	   	
+   }
 }
